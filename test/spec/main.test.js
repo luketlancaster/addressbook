c5720f85cf6ef5c5a0607d0e0ee708005c1ca720
Luke Lancaster <luketlancaster@gmail.com>
2015-02-09 16:13:24 -0600

    add public dir, index.html, test dir, and js files as well as first tests

---
+++ test/spec/main.test.js
@@ -0,0 +1,13 @@
+describe('test suite', function () {
+  it('should assert true', function () {
+    true.should.be.true;
+    false.should.be.false;
+  });
+});
+
+describe('hello', function() {
+  it('should return world', function() {
+    hello().should.equal('world');
+  });
+});
+
