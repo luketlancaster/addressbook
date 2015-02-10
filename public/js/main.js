ac12bc2c67124b48269c96a9e1e6652298e7c408
Luke Lancaster <luketlancaster@gmail.com>
2015-02-09 22:26:18 -0600

    add object creation on button click, showing and hiding of div, and table creation

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,58 @@
+/* jshint jquery: true */
+
+//$(document).ready(initialize);
+
+var firebaseUrl = 'https://c8addressbook.firebaseio.com/.json';
+
 function hello() {
   return 'world';
 }
+
+function addRowsToTable(contact) {
+  var $row = $('<tr></tr>');
+
+  $row.append('<td>' + contact.firstName + '</td>');
+  $row.append('<td>' + contact.lastName + '</td>');
+  $row.append('<td>' + contact.phoneNumber + '</td>');
+  $row.append('<td>' + contact.email + '</td>');
+  $row.append('<td>' + contact.twitter + '</td>');
+  $row.append('<td>' + contact.photoUrl + '</td>');
+
+  $('tbody').append($row);
+
+  return $row;
+}
+
+
+$('#addContact').click(sendData);
+$('#newContact').click(showForm);
+
+
+function showForm() {
+  $('.contact-info-box').css('visibility', 'visible');
+}
+
+function sendData(evt) {
+  evt.preventDefault();
+
+  var $firstName = $('#firstName').val(),
+      $lastName = $('#lastName').val(),
+      $phoneNumber = $('#phoneNumber').val(),
+      $email = $('#email').val(),
+      $twitter = $('#twitter').val(),
+      $photoUrl = $('#photoUrl').val();
+
+  var contact = { firstName: $firstName,
+                  lastName: $lastName,
+                  phoneNumber: $phoneNumber,
+                  email: $email,
+                  twitter: $twitter,
+                  photoUrl: $photoUrl };
+
+  var data = JSON.stringify(contact);
+
+  addRowsToTable(contact);
+
+}
+
+
--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -1,3 +1,5 @@
+/* jshint mocha: true, expr: true, strict: false */
+
 describe('test suite', function () {
   it('should assert true', function () {
     true.should.be.true;
@@ -10,4 +12,21 @@ describe('hello', function() {
     hello().should.equal('world');
   });
 });
+
+describe('addRowsToTable', function() {
+  it('should add rows to the table', function() {
+    var contact = { FirstName: 'Luke', LastName: 'Lancaster', PhoneNumber: '615-715-5906' };
+    $('tr').length.should.equal(0);
+    addRowsToTable(contact);
+    $('tr').length.should.equal(1);
+  });
+});
+
+describe('createContactObject', function() {
+  it('should create an object with values from the inputs', function() {
+    createContactObject(contact);
+    
+  });
+});
+
 
