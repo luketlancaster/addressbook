29c089f28008bf76ae49b17528fcbe288637fd9c
Luke Lancaster <luketlancaster@gmail.com>
2015-02-11 09:49:33 -0600

    add if satement for logged in users and script for firebase

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,29 @@
 /* jshint jquery: true */
 
 'use strict';
-var firebaseUrl = 'https://c8addressbook.firebaseio.com/contacts.json';
+var FIREBASE_URL = 'https://c8addressbook.firebaseio.com',
+    fb           = new Firebase(FIREBASE_URL),
+    usersFbUrl;
+
 //Used for testing
 function hello() {
   return 'world';
 }
 
 //Get the saved data from firebase and add rows with
-$.get(firebaseUrl, function(res) {
-  Object.keys(res).forEach(function(uuid) {
-    addRowsOnLoad(uuid, res[uuid]);
+
+if (fb.getAuth()) {
+  $('.login').remove();
+  $('.app').toggleClass('hidden');
+
+  usersFbUrl = FIREBASE_URL + '/users/' + fb.getAuth().uid + '/data';
+
+  $.get(usersFbUrl + '/contacts.json', function(res) {
+    Object.keys(res).forEach(function(uuid) {
+      addRowsOnLoad(uuid, res[uuid]);
+    });
   });
-});
+}
 
 //Next two for adding rows, either on load or for new data
 function addRowsOnLoad(uuid, contact) {
@@ -111,3 +122,5 @@ function sendData(evt) {
 function addUUID(res) {
   $('tr').last('tr').attr('data-uuid', res.name);
 }
+
+
